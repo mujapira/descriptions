c5d6fcb20cc78ec7666356d9be5c25211969cf63
mujapiraa2w <mauricio@a2web.com.br>
2022-04-26 14:39:42 -0300

    slider central sincronizado e responsivo

--- index.php
+++ index.php
@@ -69,30 +69,60 @@ require_once './inc/functions/functions.php';
                 <div class="swiper-button-next"></div>
                 <div class="swiper-button-prev"></div>
             </div>
-            <div thumbsSlider="" class="swiper mySwiper2Thumb">
-                <div class="text">
-                    <img class="bar" src="./assets/_opcao01/lp_tenis_extreme_blade_1280px-01_0001s_0000s_0000_traço.png" alt="">
-                    <p>Se você está procurando um Tênis que cai
-                        perfeito no seu dia a dia e é excelente para
-                        ter um bom desempenho na academia, o
-                        <b> Tênis Extreme Blade é o seu par perfeito.</b>
-                    </p>
+
+            <div class="wrapper">
+
+                <div class="swiper mySwiper2Thumb2">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <p>Se você está procurando um Tênis que cai
+                                perfeito no seu dia a dia e é excelente para
+                                ter um bom desempenho na academia, o
+                                <b> Tênis Extreme Blade é o seu par perfeito.</b>
+                            </p>
+                        </div>
+                        <div class="swiper-slide">
+                            <p> Com um acabamento de alta qualidade,
+                                design arrojado e moderno, não importa qual o seu estilo,
+                                pois o<b> Extreme Blade se encaixa no seu.</b>
+                            </p>
+                        </div>
+                        <div class="swiper-slide">
+                            <p>Se você está procurando um Tênis que cai
+                                perfeito no seu dia a dia e é excelente para
+                                ter um bom desempenho na academia, o
+                                <b> Tênis Extreme Blade é o seu par perfeito.</b>
+                            </p>
+                        </div>
+                        <div class="swiper-slide">
+                            <p> Com um acabamento de alta qualidade,
+                                design arrojado e moderno, não importa qual o seu estilo,
+                                pois o<b> Extreme Blade se encaixa no seu.</b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
+
                 </div>
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0000_foto_01.png">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0005_foto_06.png">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0000_foto_01.png">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0005_foto_06.png">
+                <div thumbsSlider="" class="swiper mySwiper2Thumb">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0000_foto_01.png">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0005_foto_06.png">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0000_foto_01.png">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/_opcao03/lp_tenis_extreme_blade_1280px-03_0002s_0001s_0005_foto_06.png">
+                        </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
         <div class="container-02">
             <img class="bar" src="./assets/_opcao01/lp_tenis_extreme_blade_1280px-01_0001s_0000s_0000_traço.png">
@@ -144,7 +174,7 @@ require_once './inc/functions/functions.php';
     <script>
         $(document).ready(function() {
 
-            var swiper = new Swiper("#line-01 .mySwiper1", {
+            var swiper1 = new Swiper("#line-01 .mySwiper1", {
                 loop: true,
                 pagination: {
                     el: ".swiper-pagination",
@@ -155,26 +185,44 @@ require_once './inc/functions/functions.php';
                 },
             });
 
-            var swiper = new Swiper(".mySwiper2Thumb", {
+
+            var swiperText = new Swiper(".mySwiper2Thumb2", {
+                slidesPerView: 1,
+                freeMode: true,
+                watchSlidesProgress: true,
+                loop: true,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                    renderBullet: function(index, className) {
+                        return '<span class="' + className + '">' + (index + 1) + "</span>";
+                    },
+                },
+            });
+
+            var swiperThumb = new Swiper(".mySwiper2Thumb", {
                 spaceBetween: 10,
                 slidesPerView: 4,
                 freeMode: true,
                 watchSlidesProgress: true,
             });
 
-            var swiper = new Swiper(".mySwiper2Main", {
+            var swiperMain = new Swiper(".mySwiper2Main", {
                 loop: true,
                 spaceBetween: 10,
+                multipleActiveThumbs: true,
                 navigation: {
                     nextEl: ".swiper-button-next",
                     prevEl: ".swiper-button-prev",
                 },
                 thumbs: {
-                    swiper: swiper,
+                    swiper: swiperThumb,
                 },
             });
 
-            var swiper = new Swiper(".mySwiper3", {
+            swiperMain.controller.control = swiperText;
+
+            var swiper3 = new Swiper(".mySwiper3", {
                 effect: "coverflow",
                 grabCursor: true,
                 centeredSlides: true,
