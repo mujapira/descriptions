8f03c7bc16c7027c7959fb8aebf7d1579bd78ded
Mujapira <malmasmello2@gmail.com>
2022-06-17 11:18:54 -0300

    first commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once './inc/functions/functions.php';
 
 <head>
     <title>Descrição </title>
-    <base href="<?= PATH_URL ?>" />
+    
     <meta charset=" utf-8">
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, user-scalable=0">
